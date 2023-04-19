5455cf6cddad17d464e875597eedfc6fdefa288d
Rohan Sharma <onlyrohan21@gmail.com>
2023-04-19 07:35:58 +0530

    recieve-1

--- src/scenes/wallet/index.jsx
+++ src/scenes/wallet/index.jsx
@@ -1,4 +1,6 @@
-import { Box, Button,Typography, useTheme } from "@mui/material";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Button,Divider,Typography,TextField, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { mockDataCoin} from "../../data/mockData";
@@ -6,12 +8,72 @@ import AddIcon from '@mui/icons-material/Add';
 import Header from "../../components/Header";
 import CallReceivedOutlinedIcon from '@mui/icons-material/CallReceivedOutlined';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import { styled } from '@mui/material/styles';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import LooksOneRoundedIcon from '@mui/icons-material/LooksOneRounded';
+import { CenterFocusStrong } from '@mui/icons-material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+// const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+//   '& .MuiDialogContent-root': {
+//     padding: theme.spacing(2),
+//   },
+//   '& .MuiDialogActions-root': {
+//     padding: theme.spacing(1),
+//   },
+// }));
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
 
 
-const Team = () => {
+const Wallet = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [open, setOpen] = React.useState(false);
+  const [address, setAdd] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleAddress = () => {
+    setAdd(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   // to diable the default behaviour of selecting the row
   const handleClick = (event, cellValues) => {
     console.log(cellValues.row);
@@ -60,9 +122,8 @@ const Team = () => {
           <Button
             variant="contained"
             color="primary"
-            onClick={(event) => {
-              handleClick(event, cellValues);
-            }}
+            onClick={handleClickOpen}
+            
           >
            <CallReceivedOutlinedIcon style={{color:colors.blueAccent[200]}}/>  Receive
           </Button>
@@ -140,9 +201,75 @@ const Team = () => {
       >
         <DataGrid rows={mockDataCoin} columns={columns} onCellClick={handleCellClick}
         onRowClick={handleRowClick}/>
+        
+        <Dialog
+         PaperProps={{ sx: { backgroundColor: colors.primary[900],width: "80%", height: "90%", } }}
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+        
+      >
+       
+        <Box width="600px" p={2}>
+        
+        <Typography style={{ fontWeight: 500,fontSize:40 ,marginLeft:200}}>
+            Recieve
+            <CloseIcon style={{ marginLeft:200}}autoFocus onClick={handleClose}>
+            Close
+          </CloseIcon>
+          </Typography>
+          <img  width="550px"src={require('../../logos/dev.png')}></img>
+          <Box marginLeft={5} >
+        <Typography style={{fontSize:25 , fontWeight: 300}}>
+            Follow the instructions on device
+          </Typography>
+          <Box
+          height={35}
+          width= {450}
+               backgroundColor={colors.yellow[700]}
+               borderRadius={2}
+              >
+          <Typography style={{ fontSize:20 ,marginTop: 20}}>
+          <ArrowForwardIcon style={{ marginLeft:5,color: colors.yellow[900]}}/>   Select the Wallet On Device
+          </Typography>
+          </Box>
+          <Box
+          height={35}
+          width= {450}
+          
+               backgroundColor={colors.yellow[700]}
+               borderRadius={2}
+              >
+          <Typography style={{ fontSize:20 ,marginTop: 20}}>
+          <ArrowForwardIcon style={{ marginLeft:5,color: colors.yellow[900]}}/>  Select the Coin on Device
+          </Typography>
+          </Box>
+          <Box
+          height={35}
+          width= {450}
+               backgroundColor={colors.yellow[700]}
+               borderRadius={2}
+              >
+          <Typography style={{ fontSize:20 ,marginTop: 20}}>
+          <ArrowForwardIcon style={{marginLeft:5,color: colors.yellow[900]}}/>  Tap 1 card of any 4 Cards
+          </Typography>
+          </Box>
+          </Box>
+       
+       <Divider style={{marginTop:20,width:599,marginLeft:-15}}/>
+          <Button onClick={handleAddress} style={{fontSize:23,marginTop: 30, marginLeft: 400, color: colors.yellow[900]}} variant="outlined"  >
+            Continue
+          </Button>
+          
+        </Box>
+        <DialogActions>
+          
+        </DialogActions>
+      </Dialog>
+        
       </Box>
     </Box>
   );
 };
 
-export default Team;
+export default Wallet;
